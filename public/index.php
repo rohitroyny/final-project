b9b58956f942a39117bc9948f9bbc532ae9e3d49
rohitroyny <157422306+rohitroyny@users.noreply.github.com>
2024-04-13 14:11:27 -0400

    Update index.php
    Clarified Comments: Each require and operation is documented to describe its purpose clearly. This helps in understanding the flow of the program and the role of each included file.
    
    Environment Configuration: The loading of .env variables is explicitly mentioned, ensuring that you know where configuration settings are coming from.
    
    Router Initialization: The router is instantiated with routes defined in routes.php, which is a critical operation as it determines how HTTP requests are handled in your application.

--- public/index.php
+++ public/index.php
@@ -1,16 +1,26 @@
 <?php
 
+// Load the initialization script that sets up autoloaders and basic configuration
 require_once '../app/core/init.php';
+
+// Load routes configuration
 require_once '../app/core/routes.php';
+
+// Load environment variables from .env file for configuration
 $env = parse_ini_file('../.env');
+
+// Load application configurations
 require '../app/core/config.php';
 
+// Import necessary classes
 use app\core\Router;
 
+// Load helper functions specific to Vite and asset handling
 require_once __DIR__ . '/helpers.php';
 
-//to add a new route add to the app/core/routes.php array
+// Initialize the router with predefined routes from routes.php
 $router = new Router($routes);
+
+// Serve the current route based on request
 $router->serveRoute();
 ?>
-
